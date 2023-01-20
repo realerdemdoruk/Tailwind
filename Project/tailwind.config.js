f582a5da105f3c84f0a39b50329343023eb84799
Erdem <erdemdoruk3@gmail.com>
2023-01-20 19:45:18 +0300

    Header eklendi

---
+++ Project/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    // Eğer container'ın içeriğini ortalamak istiyorsanız aşağıdaki kodu kullanın. mx-auto kullanmaya gerek yok.
+    container: {
+      center: true,
+    },
+
+    extend: {
+      fontFamily: {
+        gemunu: ["Gemunu Libre", "sans-serif"],
+        open: ["Open Sans", "sans-serif"],
+      },
+
+      colors: {
+        "gega-red": "#BC1A45",
+        "gega-melon": "#FFD369",
+        "gega-grey": "#DDDDDD",
+        "gega-white": "#F7F7F7",
+      },
+    },
+  },
+  plugins: [],
+};
